311c6ddef4c67ffb85dc8f775b7ffaa65e23d3ae
Maksim Ivanovskij <maksim.ivanovskij@fitek.com>
2023-10-06 15:41:26 +0300

    File list refresh and outbox delay settings.

--- EuroConnector.ClientApp/Data/Services/VersionService.cs
+++ EuroConnector.ClientApp/Data/Services/VersionService.cs
@@ -19,6 +19,11 @@ namespace EuroConnector.ClientApp.Data.Services
 		public async Task<ApiVersion> GetApiVersion()
 		{
 			var apiUrl = await _localStorage.GetItemAsync<string>("apiUrl");
+			if (string.IsNullOrEmpty(apiUrl)) return new ApiVersion
+			{
+				Version = "Error",
+				ReleaseDate = null,
+			};
 
 			var result = await _httpClient.GetFromJsonAsync<ApiVersion>($"{apiUrl}public/v1/extensions/version");
 
