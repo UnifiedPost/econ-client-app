11b96a0f3aa650a09fd2fa2613a859100a6f5695
Maksim Ivanovskij <maksim.ivanovskij@fitek.com>
2023-10-09 15:42:32 +0300

    Updated to .NET 7, added logging.

--- EuroConnector.ClientApp/MauiProgram.cs
+++ EuroConnector.ClientApp/MauiProgram.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.Extensions.Configuration;
 using MudBlazor;
 using MudBlazor.Services;
+using Serilog;
 using System.Reflection;
 using Toolbelt.Blazor.Extensions.DependencyInjection;
 
@@ -14,58 +15,71 @@ namespace EuroConnector.ClientApp;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.UseMauiCommunityToolkit()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
-		var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-		builder.Configuration.AddConfiguration(config);
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Verbose()
+            .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
+            .MinimumLevel.Override("MudBlazor", Serilog.Events.LogEventLevel.Warning)
+            .Enrich.FromLogContext()
+            .WriteTo.File(Path.Combine(path, "logs", "log.txt"), rollingInterval: RollingInterval.Day)
+            .CreateLogger();
 
-		builder.Services.AddMauiBlazorWebView();
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            })
+            .Logging.AddSerilog(dispose: true);
+
+        var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        builder.Configuration.AddConfiguration(config);
+
+        builder.Services.AddMauiBlazorWebView();
 #if DEBUG
-		builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Services.AddBlazorWebViewDeveloperTools();
 #endif
 
-		builder.Services.AddBlazoredLocalStorage();
+        builder.Services.AddBlazoredLocalStorage();
 
-		builder.Services.AddMudServices(config =>
-		{
-			config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+        builder.Services.AddMudServices(config =>
+        {
+            config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
 
-			config.SnackbarConfiguration.PreventDuplicates = false;
-			config.SnackbarConfiguration.NewestOnTop = false;
-			config.SnackbarConfiguration.ShowCloseIcon = true;
-			config.SnackbarConfiguration.VisibleStateDuration = 7500;
-			config.SnackbarConfiguration.ShowTransitionDuration = 150;
-			config.SnackbarConfiguration.HideTransitionDuration = 150;
-			config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
-		});
+            config.SnackbarConfiguration.PreventDuplicates = false;
+            config.SnackbarConfiguration.NewestOnTop = false;
+            config.SnackbarConfiguration.ShowCloseIcon = true;
+            config.SnackbarConfiguration.VisibleStateDuration = 7500;
+            config.SnackbarConfiguration.ShowTransitionDuration = 150;
+            config.SnackbarConfiguration.HideTransitionDuration = 150;
+            config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+        });
 
-		builder.Services.AddScoped(sp => new HttpClient().EnableIntercept(sp));
+        builder.Services.AddScoped(sp => new HttpClient().EnableIntercept(sp));
 
-		builder.Services.AddScoped<AuthenticationProvider>();
-		builder.Services.AddScoped<AuthenticationStateProvider>(provider => provider.GetService<AuthenticationProvider>());
-		builder.Services.AddAuthorizationCore();
+        builder.Services.AddScoped<AuthenticationProvider>();
+        builder.Services.AddScoped<AuthenticationStateProvider>(provider => provider.GetService<AuthenticationProvider>());
+        builder.Services.AddAuthorizationCore();
 
-		builder.Services.AddScoped<ThemeProvider>();
+        builder.Services.AddScoped<ThemeProvider>();
 
-		builder.Services.AddSingleton<TimerProvider>();
+        builder.Services.AddSingleton<TimerProvider>();
 
-		builder.Services.AddScoped<IVersionService, VersionService>();
-		builder.Services.AddScoped<ISetupService, SetupService>();
-		builder.Services.AddScoped<IDocumentService, DocumentService>();
-		builder.Services.AddScoped<IRefreshTokenService, RefreshTokenService>();
-		builder.Services.AddScoped<HttpInterceptorService>();
+        builder.Services.AddScoped<IVersionService, VersionService>();
+        builder.Services.AddScoped<ISetupService, SetupService>();
+        builder.Services.AddScoped<IDocumentService, DocumentService>();
+        builder.Services.AddScoped<IRefreshTokenService, RefreshTokenService>();
+        builder.Services.AddScoped<HttpInterceptorService>();
 
-		builder.Services.AddHttpClientInterceptor();
+        builder.Services.AddHttpClientInterceptor();
 
-		return builder.Build();
-	}
+        Log.Information("Starting Application");
+
+        return builder.Build();
+    }
 }
