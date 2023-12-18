e0176b46fcc83785d60258332affcc2d63af99b4
Maksim Ivanovskij <maksim.ivanovskij@fitek.com>
2023-12-18 11:03:59 +0200

    Added global error boundaries for handling unhandled exceptions.

--- EuroConnector.ClientApp/MauiProgram.cs
+++ EuroConnector.ClientApp/MauiProgram.cs
@@ -56,7 +56,7 @@ public static class MauiProgram
         {
             var httpClient = new HttpClient().EnableIntercept(sp);
 
-            httpClient.DefaultRequestHeaders.Add("Client-App-Version", "1.2.3");
+            httpClient.DefaultRequestHeaders.Add("Client-App-Version", "1.2.4");
 
             return httpClient;
         });
