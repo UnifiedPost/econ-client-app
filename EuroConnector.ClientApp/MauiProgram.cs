3381f7e404eb1b2cc848d00f0766bd28a841e853
Maksim Ivanovskij <maksim.ivanovskij@fitek.com>
2024-02-09 10:44:26 +0200

    Removed MLR button.

--- EuroConnector.ClientApp/MauiProgram.cs
+++ EuroConnector.ClientApp/MauiProgram.cs
@@ -60,7 +60,7 @@ public static class MauiProgram
         {
             var httpClient = new HttpClient().EnableIntercept(sp);
 
-            httpClient.DefaultRequestHeaders.Add("Client-App-Version", "2.0.0-dev");
+            httpClient.DefaultRequestHeaders.Add("Client-App-Version", "2.0.0-beta");
 
             return httpClient;
         });
