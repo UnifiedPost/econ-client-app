7d13fcbc9bc1abed3676c309e3d61f7fd7533597
Maksim Ivanovskij <maksim.ivanovskij@fitek.com>
2024-04-05 09:44:58 +0300

    Invoice Response improvements.

--- EuroConnector.ClientApp/MauiProgram.cs
+++ EuroConnector.ClientApp/MauiProgram.cs
@@ -60,7 +60,7 @@ public static class MauiProgram
         {
             var httpClient = new HttpClient().EnableIntercept(sp);
 
-            httpClient.DefaultRequestHeaders.Add("Client-App-Version", "2.0.7");
+            httpClient.DefaultRequestHeaders.Add("Client-App-Version", "2.0.8");
 
             return httpClient;
         });
